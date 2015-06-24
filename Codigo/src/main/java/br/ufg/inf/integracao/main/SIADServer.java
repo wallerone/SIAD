43674909d7af7e9653a195be0949b1caf6baa320
Mauro Jr <mauro.zeroglosa@gmail.com>
2015-06-24 14:02:49 -0300

    Move para thread separada parte do bootstrap que impede que a execução do SIADServer continue

--- Codigo/src/main/java/br/ufg/inf/integracao/main/SIADServer.java
+++ Codigo/src/main/java/br/ufg/inf/integracao/main/SIADServer.java
@@ -9,6 +9,7 @@ import org.apache.http.ExceptionLogger;
 import org.apache.http.impl.nio.bootstrap.ServerBootstrap;
 import org.apache.http.impl.nio.bootstrap.HttpServer;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
@@ -17,18 +18,21 @@ import static br.ufg.inf.integracao.util.SIADDefaults.NEW_USER_ENDPOINT;
 import static br.ufg.inf.integracao.util.SIADDefaults.DELETE_USER_ENDPOINT;
 import static br.ufg.inf.integracao.util.SIADDefaults.SIAD_SERVER_STRING;
 
-public class SIADServer {
+public class SIADServer extends Thread {
 
 	private static final Logger logger = Logger.getLogger(SIADServer.class.getName());
+	private static HttpServer server;
+	private static final SIADServer instance = new SIADServer();
 
+	private SIADServer() {}
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 
 		logger.info("Preparing SIADServer...");
 		SIADSenderService.getInstance().start();
 
 		int port = 8080;
-		final HttpServer server = ServerBootstrap.bootstrap()
+		server = ServerBootstrap.bootstrap()
 				.setListenerPort(port)
 				.setServerInfo(SIAD_SERVER_STRING)
 				.setExceptionLogger(ExceptionLogger.STD_ERR)
@@ -37,8 +41,7 @@ public class SIADServer {
 				.registerHandler(DELETE_USER_ENDPOINT, new SIADUnregisterUserHandler())
 				.create();
 
-		server.start();
-		server.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+		instance.start();
 
 		logger.info("Endpoint for new messages: " + NEW_MESSAGE_ENDPOINT);
 		logger.info("Endpoint for new users: " + NEW_USER_ENDPOINT);
@@ -52,5 +55,15 @@ public class SIADServer {
 				server.shutdown(5, TimeUnit.SECONDS);
 			}
 		});
+	}
+
+	@Override
+	public void run() {
+		try {
+			server.start();
+			server.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+		} catch (InterruptedException | IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
